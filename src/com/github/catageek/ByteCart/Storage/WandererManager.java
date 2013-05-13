9e87fc1b21ef7921bc9f1645b41df0070387a3d5
catageek <catageek@free.fr>
2013-05-13 20:09:29 +0200

    Fix timeout bug causing NullPointerException when updater stays more than 1 hour in network

--- src/com/github/catageek/ByteCart/Storage/WandererManager.java
+++ src/com/github/catageek/ByteCart/Storage/WandererManager.java
@@ -1,10 +1,16 @@
 package com.github.catageek.ByteCart.Storage;
 
+import com.github.catageek.ByteCart.ByteCart;
+
 public final class WandererManager {
 
 	// entries stay for 1h
-	private final ExpirableMap<Integer, Integer> WandererMap = new ExpirableMap<Integer, Integer>(72000, false, "WandererMap");
+	private final ExpirableMap<Integer, Integer> WandererMap;
 
+	public WandererManager() {
+		long duration = ByteCart.myPlugin.getConfig().getInt("updater.timeout", 60)*1200;
+		WandererMap = new ExpirableMap<Integer, Integer>(duration, false, "WandererMap");
+	}
 	public boolean isWanderer(int id) {
 		return WandererMap.contains(id);
 	}
