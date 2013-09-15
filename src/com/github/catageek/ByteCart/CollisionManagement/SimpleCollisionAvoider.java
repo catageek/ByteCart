89cd7bb48a47444dc43d4f8dd3c5247e4ae091d2
catageek <catageek@free.fr>
2013-09-15 11:34:43 +0200

    Fix lever state to be inversed when active lever is the second one

--- src/com/github/catageek/ByteCart/CollisionManagement/SimpleCollisionAvoider.java
+++ src/com/github/catageek/ByteCart/CollisionManagement/SimpleCollisionAvoider.java
@@ -2,6 +2,7 @@ package com.github.catageek.ByteCart.CollisionManagement;
 
 import org.bukkit.Location;
 
+import com.github.catageek.ByteCart.ByteCart;
 import com.github.catageek.ByteCart.HAL.RegistryOutput;
 import com.github.catageek.ByteCart.Signs.Triggable;
 import com.github.catageek.ByteCart.Storage.ExpirableMap;
@@ -11,7 +12,7 @@ public class SimpleCollisionAvoider extends AbstractCollisionAvoider implements 
 	private static final ExpirableMap<Location, Boolean> recentlyUsedMap = new ExpirableMap<Location, Boolean>(20, false, "recentlyUsed9000");
 	private static final ExpirableMap<Location, Boolean> hasTrainMap = new ExpirableMap<Location, Boolean>(14, false, "hastrain");
 
-	private RegistryOutput Lever1 = null, Lever2 = null;
+	private RegistryOutput Lever1 = null, Lever2 = null, Active = null;
 	private final Location loc1;
 
 	private Side state;
@@ -39,52 +40,76 @@ public class SimpleCollisionAvoider extends AbstractCollisionAvoider implements 
 
 	public SimpleCollisionAvoider(Triggable ic, org.bukkit.Location loc) {
 		super(loc);
-/*		if(ByteCart.debug)
+		if(ByteCart.debug)
 			ByteCart.log.info("ByteCart: new SimpleCollisionAvoider() at " + loc);
-*/		Lever1 = ic.getOutput(0);
+
+		Lever1 = ic.getOutput(0);
+		Active = Lever1;
 		loc1 = ic.getLocation();
 		state = (Lever1.getAmount() == 0 ? Side.LEFT : Side.RIGHT);
 	}
 
 	public Side WishToGo(Side s, boolean isTrain) {
 
-/*		if(ByteCart.debug)
-			ByteCart.log.info("ByteCart : WishToGo to side " + s + " and isTrain is " + isTrain);
+		Side trueside = getTrueSide(s);
+
+		if(ByteCart.debug)
+			ByteCart.log.info("ByteCart : WishToGo to side " + trueside + " and isTrain is " + isTrain);
 		if(ByteCart.debug)
 			ByteCart.log.info("ByteCart : state is " + state);
 		if(ByteCart.debug)
 			ByteCart.log.info("ByteCart : recentlyUsed is " + this.getRecentlyUsed() + " and hasTrain is " + this.getHasTrain());
 		if(ByteCart.debug)
 			ByteCart.log.info("ByteCart : Lever1 is " + Lever1.getAmount());
-*/
-		if ( s != state && (Lever2 == null || ( !this.getRecentlyUsed()) && !this.getHasTrain())) {
-			Set(s);
+
+		if ( trueside != state 
+				&& (Lever2 == null 
+				|| ( !this.getRecentlyUsed()) && !this.getHasTrain())) {
+			Set(trueside);
 		}
 		this.setRecentlyUsed(true);
 		return state;
 
 	}
 
+	/**
+	 * Get the fixed side if it is the second lever
+	 * 
+	 * @param s the original side
+	 * @return the fixed side
+	 */
+	private Side getTrueSide(Side s) {
+		Side trueside = (Active == Lever2 ? s.opposite() : s);
+		return trueside;
+	}
+
 
 	@Override
 	public void Add(Triggable t) {
-		if (t.getLocation().equals(loc1))
+		if (t.getLocation().equals(loc1)) {
+			Active = Lever1;
 			return;
+		}
+		if (Lever2 != null) {
+			Active = Lever2;
+			return;
+		}
 		Lever2 = t.getOutput(0);
-		Lever2.setAmount(state.opposite().Value());
-/*		if(ByteCart.debug)
+		Active = Lever2;
+		Lever2.setAmount(getTrueSide(state).Value());
+		if(ByteCart.debug)
 			ByteCart.log.info("ByteCart: Add and setting lever2 to " + Lever2.getAmount());
-*/	}
+	}
 
 	private void Set(Side s) {
 		this.Lever1.setAmount(s.Value());
-/*		if(ByteCart.debug)
+		if(ByteCart.debug)
 			ByteCart.log.info("ByteCart: Setting lever1 to " + Lever1.getAmount());
-*/		if (this.Lever2 != null) {
+		if (this.Lever2 != null) {
 			this.Lever2.setAmount(s.opposite().Value());
-/*			if(ByteCart.debug)
+			if(ByteCart.debug)
 				ByteCart.log.info("ByteCart: Setting lever2 to " + Lever2.getAmount());
-*/		}
+		}
 		state = s;
 	}
 
